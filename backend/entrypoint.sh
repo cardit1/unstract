3c027ffe56627649ca46956cf72f5becd8a9fb99
cardit1 <maneesh@cardit.in>
2025-02-03 09:08:21 +0000

    backlend in dev mode builidnfrom local

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,13 +7,13 @@ if [ "$cmd" = "migrate" ]; then
 fi
 
 # NOTE: Leaving below for reference incase required in the future
-# python manage.py runserver 0.0.0.0:8000 --insecure
+python manage.py runserver 0.0.0.0:8000 --insecure
 # NOTE updated socket threads
-.venv/bin/gunicorn \
-    --bind 0.0.0.0:8000 \
-    --workers 2 \
-    --threads 2 \
-    --log-level debug \
-    --timeout 600 \
-    --access-logfile - \
-    backend.wsgi:application
+# .venv/bin/gunicorn \
+#     --bind 0.0.0.0:8000 \
+#     --workers 2 \
+#     --threads 2 \
+#     --log-level debug \
+#     --timeout 600 \
+#     --access-logfile - \
+#     backend.wsgi:application
